66726e13c196578de2d0e40ced6dc7b3ee5f281e
nathanielmcdowell <nathaniel29651@gmail.com>
2020-10-25 07:46:43 -0400

    Initial Commit

---
+++ customSubscripts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  customSubscripts
+//
+//  Created by Nathaniel Mcdowell on 10/25/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ customSubscripts/customSubscriptsApp.swift
@@ -0,0 +1,17 @@
+//
+//  customSubscriptsApp.swift
+//  customSubscripts
+//
+//  Created by Nathaniel Mcdowell on 10/25/20.
+//
+
+import SwiftUI
+
+@main
+struct customSubscriptsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
